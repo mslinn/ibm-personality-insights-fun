4f27f3a52efc41d67dc5802c215a8906208c8564
Mike Slinn <mslinn@micronauticsresearch.com>
2018-09-05 23:44:40 -0700

    -

--- example1.py
+++ example1.py
@@ -9,18 +9,23 @@ from bs4 import Comment
 import re
 
 def cleanSoup(html):
-  # from https://stackoverflow.com/a/49440655/553865
   soup = BeautifulSoup(html, "html5lib")
-  [x.extract() for x in soup.find_all('script')]
-  [x.extract() for x in soup.find_all('style')]
-  [x.extract() for x in soup.find_all('meta')]
-  [x.extract() for x in soup.find_all('noscript')]
-  [x.extract() for x in soup.find_all('div', id='disqus_thread')]
-  [x.extract() for x in soup.find_all(text=lambda text:isinstance(text, Comment))]
+
+  for tag in ['script', 'style', 'meta', 'noscript']:
+    for element in soup.find_all(tag):
+      element.decompose()
+
+  for element in soup.find_all('div', id='disqus_thread'):
+    element.decompose()
+
+  for element in soup.find_all('a', class_='dsq-brlink'):
+    element.decompose()
+  #for element in soup.find_all(text=lambda text:isinstance(text, Comment)):
+    #element.decompose()
   return soup
 
 def fetch_posting(url):
-  "Returns contents of <div class='container'></div>"
+  """Returns contents of <div class='container'></div>"""
   page = requests.get(url)
   soup = cleanSoup(page.content)
   try:
@@ -34,7 +39,8 @@ def fetch_posting(url):
       print('Error code: ', e.code)
       return ""
   else: # everything is fine
-    return re.sub("(\t| )+" , " ", re.sub("(\n|\r)+" , "\n", container))
+    return re.sub("(\t| )+" , " ", re.sub("(\n|\r)+" , "\n", container)).strip()
 
+#fetch_posting('https://blog.mslinn.com/blog/2017/10/15/61')
 print(fetch_posting('https://blog.mslinn.com/blog/2017/10/15/61'))
-print(fetch_posting('https://blog.mslinn.com/blog/2008/04/28/cult-of-software-god'))
+#print(fetch_posting('https://blog.mslinn.com/blog/2008/04/28/cult-of-software-god'))
