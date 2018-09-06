e9954fa87c45d2bb0ea740da16d9d3eee245a726
Mike Slinn <mslinn@micronauticsresearch.com>
2018-09-06 14:40:43 -0700

    -

--- example1.py
+++ example1.py
@@ -3,12 +3,12 @@
 # pip install lxml requests html5lib
 # pip install --upgrade watson-developer-cloud
 
+from __future__ import print_function
 import os, re, requests
 from requests.auth import HTTPBasicAuth
 from requests.auth import HTTPDigestAuth
 from bs4 import BeautifulSoup, Comment
 from watson_developer_cloud import DiscoveryV1
-from __future__ import print_function
 import json
 from os.path import join, dirname
 from watson_developer_cloud import PersonalityInsightsV3
@@ -45,40 +45,38 @@ def fetch_posting(url):
   else: # everything is fine
     return re.sub("(\t| )+" , " ", re.sub("(\n|\r)+" , "\n", container)).strip()
 
-def insights(text):
-  """See https://github.com/watson-developer-cloud/python-sdk"""
+def insights(user_text):
+  """ Analyze text using IBM Personality Insights.
 
-  #pi_url = "https://gateway.watsonplatform.net/personality-insights/api/v3/profile?version=2017-10-13"
+  ``pip install --upgrade watson-developer-cloud``
 
-  pi_username = os.environ.get("PI_USERNAME")
-  pi_password = os.environ.get("PI_PASSWORD")
-  #print("pi_username=" + pi_username)
-  #print("pi_password=" + pi_password)
+  This code is based on the `IBM Watson Python SDK`_ docs.
+  Environment variables must be set with Personality Insights authentication data before this function is invoked.
 
-  personality_insights = PersonalityInsightsV3(
-    version='2016-10-20',
-    ## url is optional, and defaults to the URL below. Use the correct URL for your region.
-    # url='https://gateway.watsonplatform.net/personality-insights/api',
-    username='YOUR SERVICE USERNAME',
-    password='YOUR SERVICE PASSWORD')
+  Where do I get or generate an API key? The `IBM API KEY`_ docs are confusing.
 
-    ## If service instance provides API key authentication
-    # personality_insights = PersonalityInsightsV3(
-    #     version='2016-10-20',
-    #     ## url is optional, and defaults to the URL below. Use the correct URL for your region.
-    #     url='https://gateway.watsonplatform.net/personality-insights/api',
-    #     iam_api_key='your_api_key')
+  .. _IBM Watson Python SDK:
+        https://github.com/watson-developer-cloud/python-sdk
+  .. _IBM API KEY:
+        https://console.bluemix.net/docs/resources/service_credentials.html#service_credentials
+  """
 
-    with open(join(dirname(__file__), '../resources/personality-v3.json')) as profile_json:
-    profile = personality_insights.profile(
-      profile_json.read(), content_type='application/json',
-      raw_scores=True, consumption_preferences=True)
+  insights = PersonalityInsightsV3(
+    version = '2017-10-13',
+    username = os.environ.get("PI_USERNAME"),
+    password = os.environ.get("PI_PASSWORD")
+    #iam_api_key = os.environ.get("PI_API_KEY")
+  )
 
-    print(json.dumps(profile, indent=2))
-    return profile
+  return insights.profile(
+    user_text,
+    content_type = 'text/plain;charset=utf-8',
+    raw_scores = True,
+    consumption_preferences = True
+  )
 
 posting1 = fetch_posting('https://blog.mslinn.com/blog/2017/10/15/61')
 posting2 = fetch_posting('https://blog.mslinn.com/blog/2008/04/28/cult-of-software-god')
 combined_postings = posting1 + posting2
-#print(combined_postings)
-print(insights(combined_postings))
+insightful_json = insights(combined_postings)
+print(json.dumps(insightful_json, indent=2))
--- pi_test.py
+++ pi_test.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-# pip install --upgrade watson-developer-cloud
-
+import json
 import os
+
 from watson_developer_cloud import PersonalityInsightsV3
 
-def insights(text):
+
+def insights(user_text):
   """ Analyze text using IBM Personality Insights.
+
+  ``pip install --upgrade watson-developer-cloud``
 
   This code is based on the `IBM Watson Python SDK`_ docs.
   Environment variables must be set with Personality Insights authentication data before this function is invoked.
 
-  Where do I get an API key? The `IBM API KEY`_ docs are confusing.
+  Where do I get or generate an API key? The `IBM API KEY`_ docs are confusing.
 
   .. _IBM Watson Python SDK:
         https://github.com/watson-developer-cloud/python-sdk
@@ -21,18 +24,18 @@ def insights(text):
 
   insights = PersonalityInsightsV3(
     version = '2017-10-13',
-    #username = os.environ.get("PI_USERNAME"),
-    #password = os.environ.get("PI_PASSWORD"),
-    iam_api_key = os.environ.get("PI_API_KEY")
+    username = os.environ.get("PI_USERNAME"),
+    password = os.environ.get("PI_PASSWORD")
+    #iam_api_key = os.environ.get("PI_API_KEY")
   )
 
   return insights.profile(
-    text,
-    content_type = 'application/json',
+    user_text,
+    content_type = 'text/plain;charset=utf-8',
     raw_scores = True,
     consumption_preferences = True
   )
 
-text = """This text is too short and will cause a warning"""
-insightfulText = insights(text)
-print(json.dumps(insightfulText, indent=2))
+text = """Ipsum lorem. """ * 100
+insightful_json = insights(text)
+print(json.dumps(insightful_json, indent=2))
